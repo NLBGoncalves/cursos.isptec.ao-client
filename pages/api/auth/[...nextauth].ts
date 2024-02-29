d1d6710953bc0801bc06e9eac94c0e764b98ef2b
NLBGoncalves <nilsonlaerciobotelho123@gmail.com>
2024-02-29 18:05:11 +0100

    done

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import GithubProvider from "next-auth/providers/github";
 import FacebookProvider from "next-auth/providers/facebook";
 
 console.log(process.env.GOOGLE_CLIENT_ID,'red');
@@ -10,10 +9,6 @@ export const authOptions = {
             clientId: process.env.GOOGLE_CLIENT_ID || '',
             clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
         }),
-        /*GithubProvider({
-            clientId: process.env.GITHUB_CLIENT_ID || '',
-            clientSecret: process.env.GITHUB_CLIENT_SECRET || '',
-        }),*/
         FacebookProvider({
             clientId: process.env.FACEBOOK_CLIENT_ID || '',
             clientSecret: process.env.FACEBOOK_CLIENT_SECRET || '',
