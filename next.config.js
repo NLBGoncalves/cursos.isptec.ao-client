732adeecfc321cac9dea3ac9735e01254d836dbe
NLBGoncalves <nilsonlaerciobotelho123@gmail.com>
2024-02-29 22:21:13 +0100

    done

--- next.config.js
+++ next.config.js
@@ -3,13 +3,6 @@ const nextConfig = {
     images: {
         domains: ['res.cloudinary.com','randomuser.me', 'assets.acme.com'],
       },
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'assets.example.com',
-          port: '',
-        },
-      ],
 }
 
 module.exports = nextConfig
