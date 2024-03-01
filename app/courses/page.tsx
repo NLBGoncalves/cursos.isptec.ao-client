3f937adfccb1fe40dd5522864b5b284740d61b2f
NLBGoncalves <nilsonlaerciobotelho123@gmail.com>
2024-03-01 14:23:36 +0100

    done

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -1,34 +1,111 @@
 "use client";
-import React, { useState } from "react";
-import Heading from "../utils/Heading";
+import { useGetUsersAllCoursesQuery } from "@/redux/features/courses/coursesApi";
+import { useGetHeroDataQuery } from "@/redux/features/layout/layoutApi";
+import { useSearchParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import Loader from "../components/Loader/Loader";
 import Header from "../components/Header";
+import Heading from "../utils/Heading";
+import { styles } from "../styles/style";
+import CourseCard from "../components/Course/CourseCard";
 import Footer from "../components/Footer";
-import Coursess from "./courses";
 
 type Props = {};
 
 const Page = (props: Props) => {
-  const [open, setOpen] = useState(false);
-  const [activeItem, setActiveItem] = useState(1);
+  const searchParams = useSearchParams();
+  const search = searchParams?.get("title");
+  const { data, isLoading } = useGetUsersAllCoursesQuery(undefined, {});
+  const { data: categoriesData } = useGetHeroDataQuery("Categories", {});
   const [route, setRoute] = useState("Login");
+  const [open, setOpen] = useState(false);
+  const [courses, setcourses] = useState([]);
+  const [category, setCategory] = useState("All");
+
+  useEffect(() => {
+    if (category === "All") {
+      setcourses(data?.courses);
+    }
+    if (category !== "All") {
+      setcourses(
+        data?.courses.filter((item: any) => item.categories === category)
+      );
+    }
+    if (search) {
+      setcourses(
+        data?.courses.filter((item: any) =>
+          item.name.toLowerCase().includes(search.toLowerCase())
+        )
+      );
+    }
+  }, [data, category, search]);
+
+  const categories = categoriesData?.layout?.categories;
 
   return (
     <div>
-      <Heading
-        title="Todos os Cursos - cursos.isptec.ao"
-        description="CURSOS.ISPTEC.AO é um sistema de gerenciamento de aprendizagem para ajudar os estudantes."
-        keywords="Cálculo, Álgebra Linear, English, Quimica, Fisica etc. ..."
-      />
-      <Header
-        open={open}
-        setOpen={setOpen}
-        activeItem={activeItem}
-        setRoute={setRoute}
-        route={route}
-      />
-      <br />
-      <Coursess />
-      <Footer />
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <Header
+            route={route}
+            setRoute={setRoute}
+            open={open}
+            setOpen={setOpen}
+            activeItem={1}
+          />
+          <div className="w-[95%] 800px:w-[85%] m-auto min-h-[70vh]">
+          <Heading
+            title={"cursos.isptec.ao"}
+            description={"A plataforma CURSOS.ISPTEC.AO oferece aos alunos uma oportunidade de aprender e receber suporte dos professores."}
+            keywords={"Cálculo, Álgebra Linear, English, Quimica, Fisica etc. ..."}
+            />
+            <br />
+            <div className="w-full flex items-center flex-wrap">
+              <div
+                className={`h-[35px] ${
+                  category === "All" ? "bg-[#ffc107]" : "bg-[#5050cb]"
+                } m-3 px-3 rounded-[30px] flex items-center justify-center font-Poppins cursor-pointer`}
+                onClick={() => setCategory("All")}
+              >
+                Todos
+              </div>
+              {categories &&
+                categories.map((item: any, index: number) => (
+                  <div key={index}>
+                    <div
+                      className={`h-[35px] ${
+                        category === item.title
+                          ? "bg-[#ffc107]"
+                          : "bg-[#5050cb]"
+                      } m-3 px-3 rounded-[30px] flex items-center justify-center font-Poppins cursor-pointer`}
+                      onClick={() => setCategory(item.title)}
+                    >
+                      {item.title}
+                    </div>
+                  </div>
+                ))}
+            </div>
+            {
+                courses && courses.length === 0 && (
+                    <p className={`${styles.label} justify-center min-h-[50vh] flex items-center`}>
+                    {search ? "Nenhum curso encontrado!" : "Nenhum curso encontrado nesta categoria. Por favor, tente outro!"}
+                  </p>
+                )
+            }
+            <br />
+            <br />
+            <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-3 lg:gap-[25px] 1500px:grid-cols-4 1500px:gap-[35px] mb-12 border-0">
+              {courses &&
+                courses.map((item: any, index: number) => (
+                  <CourseCard item={item} key={index} />
+                ))}
+            </div>
+          </div>
+          <Footer />
+        </>
+      )}
     </div>
   );
 };
